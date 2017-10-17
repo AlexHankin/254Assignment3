379fa7eb1836b4ba4ab178663f4be7f61c19037d
AlexHankin <ahankin@u.rochester.edu>
2017-10-17 15:13:04 -0400

    Some minor changes

--- TesterCamel.ml
+++ TesterCamel.ml
@@ -12,62 +12,66 @@ and ast_e =
 | AST_id of string
 | AST_num of string;;
 
-
-let listofVariables = []
-
 let string_of_chars chars = 
 	let buf = Buffer.create 16 in
 	List.iter (Buffer.add_char buf) listofVariables;
-	Buffer.contents buf
+	Buffer.contents buf;;
+
+let listofVariables = [] in
 
 let rec translate (ast:ast_sl)
     :  string *  string
     (* warnings  output_program *) =
-    let cProgram = (String.concat " " ["#include <stdio.h>\n #include <stdlib.h>\nint getint() {\nint myint;\nif (scanf(\"%d\", &myint) == 1){\nreturn myint;}\nelse {\nprintf(\"The input value is not of type int\n\");\nprintf(\"Terminating program...\n\");\nexit(0);}}\nvoid putint(int n) {printf(\"%d\n\", n)}\nint main () {"; translate_sl(ast);"}"]) in
-    let ErrorList = string_of_chars listofVariables
-    return(string_of_chars, cProgram)
+    let cProgram = "#include <stdio.h>\n #include <stdlib.h>\nint getint() {\nint myint;\nif (scanf(\"%d\", &myint) == 1){\nreturn myint;}\nelse {\nprintf(\"The input value is not of type int\n\");\nprintf(\"Terminating program...\n\");\nexit(0);}}\nvoid putint(int n) {printf(\"%d\n\", n)}\nint main () {" in
+    let errorList = string_of_chars listofVariables in
+    let cProgram2 = 
+	    match ast with
+	    | [] -> String.concat " " [cProgram; " "]
+	    | x :: l -> String.concat " " [cProgram;translate_s(x); translate_sl(l)] in
+	let cProgram3 = String.concat " " [cProgram2;"}"] in
+    (errorList, cProgram2)
 
-and translate_sl (ast:ast_sl) : string = function
-	| [] -> return("")
-	| x :: l -> return(String.concat " " [translate_s(x)]; translate_sl(l))
+and translate_sl (ast:ast_s list) : string = match ast with
+	| [] -> ""
+	| x :: l -> String.concat " " [translate_s(x)]; translate_sl(l)
 
 and translate_s (ast:ast_s)
     :  string =
 	match ast with
 		| AST_error -> return ("ERROR")
-		| AST_assign (str, ex) -> return(translate_assign str ex)
-		| AST_read (str) -> return(translate_read str)
-		| AST_write (ex) -> return(translate_write ex)
-		| AST_if (ex, statl) -> return(translate_if ex statl)
-		| AST_do (statl) -> return(translate_do statl)
-		| AST_check (ex) ->	return(translate_check ex)
+		| AST_assign (str, ex) -> translate_assign str ex
+		| AST_read (str) -> translate_read str
+		| AST_write (ex) -> translate_write ex
+		| AST_if (ex, statl) -> translate_if ex statl
+		| AST_do (statl) -> translate_do statl
+		| AST_check (ex) ->	translate_check ex
 
 and translate_assign (id:string) (ex:ast_e) : string =
 	if (List.mem id listofVariables) then 
-		return((String.concat " " [id; "="; tranlate_expr(ex); ";"]))
+		String.concat " " [id; "="; tranlate_expr ex; ";"]
 	else
-		listofVariables := !listofVariables@[id]
-		return((String.concat " " ["int"; id; "="; tranlate_expr(ex); ";"]))
+		listofVariables := !listofVariables@[id];
+		String.concat " " ["int"; id; "="; tranlate_expr ex; ";"]
 
 and translate_read (id:string) : string =
-	listofVariables := !listofVariables@[id]
-	return(String.concat " " ["int"; id; ";"])
+	listofVariables := !listofVariables@[id];
+	String.concat " " ["int"; id; ";"]
 		
 and translate_if (ex:ast_e) (stat:ast_sl): string =
-	return (String.concat " " ["if(";translate_expr(ex);") {";translate_s(stat);"}"])
+	String.concat " " ["if(";translate_expr ex;") {";translate_s stat; "}"]
 
 and translate_do (stat: ast_sl): string =
-	return (String.concat " " ["while(";translate_sl(stat);"}"])
+	String.concat " " ["while(";translate_sl stat;"}"]
 
 and translate_check (ex: ast_e) : string=
-	return (String.concat " " [translate_expr(ex);") {"])
+	String.concat " " [translate_expr ex;") {"]
 
 and translate_write (id:ast_e) : string =
-	return (String.concat " " ["printf(%d\n,";translate_expr(ex);");"])
+	String.concat " " ["printf(%d\n,";translate_expr ex;");"]
 
 and translate_expr (ex:ast_e) : string = match ex with
-	| AST_binop (op, left_ex, right_ex) -> return (String.concat " " [translate_expr(left_ex);op;translate_expr(right_ex)])
-	| AST_id -> if List.mem ex listofVariables then 
-				listofVariables = List.filter (fun s -> s != ex) !listofVariables
-				return ex
-	| AST_num -> return ex
+	| AST_binop (op, left_ex, right_ex) -> String.concat " " [translate_expr left_ex ;op;translate_expr right_ex]
+	| AST_id (str) -> if List.mem str listofVariables then 
+				listofVariables = List.filter (fun s -> s != ex) !listofVariables;
+				str
+	| AST_num (str) -> str;;
